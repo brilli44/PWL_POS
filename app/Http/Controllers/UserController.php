8978c3f1f7c88e4e68cc7fc0aea8841a8a6a3d9a
Brilliantna Salsabila <sbrilliantna@gmail.com>
2024-03-11 19:52:45 +0700

    Jobsheet 4-Praktium 2.4 -Retreiving or Creating Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -41,12 +41,42 @@ class UserController extends Controller
         //  }); //->first()
 
         //JS 4 Praktikum 2.2 - Not Found Exceptions
-        $user = UserModel::where('level_id',2)->count();
-        // dd($user);
-         return view('user',['data' => $user]);
+        // $user = UserModel::where('level_id',2)->count();
+        // // dd($user);
+        //  return view('user',['data' => $user]);
 
         //coba akses model UserModel
         // $user = UserModel::all(); // ambill semua data dari tabel m_user
         // return view('user', ['data' => $user]);
+        
+        //--------Jobsheet 4 praktikum 2.4----------------------------
+            // $user = UserModel::firstOrCreate(
+            //     [
+            //         'username' => 'manager22',
+            //         'nama' => 'Manager Dua Dua',
+            //         'password' => Hash::make('12345'),
+            //         'level_id' => 2
+            //     ],
+            // );
+            // return view('user', ['data' => $user]);
+
+        // $user =UserModel::firstOrNew(
+        //     [
+        //         'username' => 'manager',
+        //         'nama' => 'Manager',
+        //     ]
+        //     );
+        //     return view('user',['data' => $user]);
+
+        $user = UserModel::firstOrNew(
+                [
+                    'username' => 'manager33',
+                    'nama' => 'Manager Tiga Tiga',
+                    'password' => Hash::make('12345'),
+                    'level_id' => 2
+                ],
+            );
+            $user->save();
+            return view('user', ['data' => $user]);
     }
 }
